beb4ea842cef1312fc95ce73d18877504db418ce
Alexandra Weaver <aweaver0124@gmail.com>
2022-08-16 20:10:02 -0500

    changed footer

--- first-texas-gc-website/src/Footer.js
+++ first-texas-gc-website/src/Footer.js
@@ -5,7 +5,7 @@ function Footer(){
     return(
         <div id="footer">
             <a href="tel: 8179647620"><p>817-964-7620</p></a>
-            <p>douglasweaver@firsttexasgeneralcontracting.com</p>
+            <p>firsttexasgeneral@gmail.com</p>
             <a href="https://www.facebook.com/weaverdouglas1"><img src={facebook_icon} alt="facebook icon" height="50px" wdith="50px" /></a>
         </div>
     )
